e10ff66c87d843aaf7c4ca0f0e373a67bd8c21c0
HankDa <muhd0416@gmail.com>
2023-05-05 15:16:31 +0100

    New Features - completed the to do list front end page - make request to news api
    TODO
    - display news list by Modal component.
    - adding intrested news to todo list.
    - adding daily routing as new tab.

--- backend/todo/serializers.py
+++ backend/todo/serializers.py
@@ -3,6 +3,12 @@ from .models import Todo
 
 
 class TodoSerializer(serializers.ModelSerializer):
+    """
+    class meta is a special keyword that allows us to define a 
+    configuration for our serializer.
+    Here we are telling the serializer to use the Todo model
+    and use the fields id, title, description, completed
+    """
     class Meta:
         model = Todo
         fields = ('id', 'title', 'description', 'completed')
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -5,6 +5,17 @@ from .models import Todo
 
 
 # Create your views here.
+
 class TodoView(viewsets.ModelViewSet):
-    serializer_class = TodoSerializer
+    """
+    The ModelViewSet class provides built-in CRUD 
+    (Create, Retrieve, Update, Delete) functionality for the model.
+
+    queryset is used to determine which objects are used by the view.
+    In this case, we want all of the objects in the Todo model.
+    We could also use .filter() to filter the objects.
+    For instance, we want all of the objects that are not completed.
+    queryset = Todo.objects.filter(completed=False)
+    """
     queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
